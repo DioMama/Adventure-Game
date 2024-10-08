ee8e93853dbc343156b07729179d551e08338eb6
DioMama <diobusinesscontact@gmail.com>
2024-10-08 23:02:21 +0900

    플레이어 움직임 구현

--- 2DGame/Assets/Scripts/Player.cs
+++ 2DGame/Assets/Scripts/Player.cs
@@ -5,16 +5,27 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 4;
     public InputAction moveAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveAction.Enable();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Debug.Log(input);
+
+        if (input.x != 0 || input.y != 0)
+        {
+            Vector3 movement = new Vector3(input.x, input.y, 0);
+            Vector3 direction = movement.normalized;
+            Debug.Log($"direction: {direction} ");
+            transform.position += direction * Time.deltaTime * speed;
+        }
     }
 }
